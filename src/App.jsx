39f06e3623e6fb87290d5cd43d52dc9ad5213965
Maria <63619962+MarijaAvramovic@users.noreply.github.com>
2026-03-16 17:37:16 -0400

    chore: add card with one pokemon

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,44 @@
-import { useState } from 'react'
+import { useState, useEffect} from 'react'
 import './App.css'
 import Scoreboard from './components/Scoreboard'
+import Card from './components/Card'
 
 function App() {
 
-  return(
- <Scoreboard />
+   const [pokemon, setPokemon] = useState(null);
 
+   useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
+      .then(res => res.json())
+      .then(data => {
+        setPokemon({
+          name: data.name,
+          image: data.sprites.front_default
+        });
+      });
+  }, []);
+
+  return(
+
+    <>
+      <Scoreboard />
+      <div>
+      {pokemon && (
+        <Card
+          name={pokemon.name}
+          image={pokemon.image}
+        />
+      )}
+    </div>
+    </>
+ 
+  
   )
   
 
 }
 
 export default App
+ 
+
+ 
