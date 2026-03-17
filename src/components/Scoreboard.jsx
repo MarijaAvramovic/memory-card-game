03b801e189d008fd14b451d11aef428a96c6c05d
Maria <63619962+MarijaAvramovic@users.noreply.github.com>
2026-03-16 20:42:09 -0400

    chore

--- src/components/Scoreboard.jsx
+++ src/components/Scoreboard.jsx
@@ -3,11 +3,13 @@ import './Scoreboard.css'
 export default function Scoreboard( { score=0, bestScore=0 }) {
     return(
         <div className='scoreboard'>
+
+             <div className='nav'>
+                <h3>MEMORY CARD GAME</h3>
+            </div>
             <div>    <div>Score: {score}</div>
             <div>Best score: {bestScore}</div></div>
-            <div className='nav'>
-                <h1>MEMORY CARD GAME</h1>
-            </div>
+            
          
         </div>
     )
